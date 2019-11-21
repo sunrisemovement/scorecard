e30deaabcaf0e1ffc48af6d31f86ae9a04db6575
brookeangel <brooke.nicole.angel@gmail.com>
2019-11-20 20:27:22 -0800

    dont be destructive so you can still visit netlify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ set -euxo pipefail
 
 npm run build
 cd build
-mv static/js/main.*.js main.js
-mv static/js/runtime-main.*.js runtime.js
-mv static/js/2.*.js 2.js
-mv static/css/main.*.css main.css
+cp static/js/main.*.js main.js
+cp static/js/runtime-main.*.js runtime.js
+cp static/js/2.*.js 2.js
+cp static/css/main.*.css main.css
