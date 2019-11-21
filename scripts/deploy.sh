44424871c26509e1e4c0dfb14cd792d6765e38dd
brookeangel <brooke.nicole.angel@gmail.com>
2019-11-20 17:50:45 -0800

    Add deploy build script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Builds minfiied assets then moves them
+# to a predictable location - for our widget
+# we don't actually want to hash our filenames
+set -euxo pipefail
+
+
+npm run build
+cd build
+mv static/js/main.*.js main.js
+mv static/css/main.*.css main.css
+rm -rf static
