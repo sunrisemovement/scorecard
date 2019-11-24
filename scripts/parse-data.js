cd050b6a6a37091d64fbb055ea5e1998f2efab6b
brookeangel <brooke.nicole.angel@gmail.com>
2019-11-24 12:16:08 -0800

    Output to a file

--- scripts/parse-data.js
+++ scripts/parse-data.js
@@ -1,6 +1,15 @@
 const parser = require('../src/data/parser.js');
 var fs = require('fs');
 
-var textByLine = fs.readFileSync('Data.csv').toString()
 
-parser(textByLine);
+async function parsed() {
+  const textByLine = fs.readFileSync('Data.csv').toString()
+  const parsed = await parser(textByLine);
+  const json = JSON.stringify(parsed, undefined, 2);
+  fs.writeFile('ParsedData.js', json, 'utf8', () => {
+    console.log("Wrote to ParsedData.js");
+    console.log(json);
+  });
+}
+
+parsed();
