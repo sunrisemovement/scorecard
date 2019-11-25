4148c7438c29cca9a6f8d3d2538c5cf35fb866c0
colekins <colekins@gmail.com>
2019-11-25 10:08:29 -0500

    Temporary solution so ScrollSpy finally works properly

--- src/tables.js
+++ src/tables.js
@@ -3,21 +3,43 @@ import Table from './table.js';
 import TotalsTable from './totalsTable.js'
 import './tables.css';
 
-const Tables = (props) => (
+function Tables (props) {
+  
+  var tables = props.scorecardData.tables.slice(0,3);
+  var gndTables = props.scorecardData.tables.slice(3)
+
+return (
     <div className="tables-container">
       {
-        props.scorecardData.tables.map(function (table, i) {
-          return <Table 
-          table = {table}
-          key = {i}
-          id = {i}
-          onClickCell={props.onClickCell}
-          onClickIcon={props.onClickIcon}
-          />
+        tables.map(function (table, i) {
+            return <Table 
+            table = {table}
+            key = {i}
+            id = {i}
+            onClickCell={props.onClickCell}
+            onClickIcon={props.onClickIcon}
+            />
         })
       }
+
+      <div id="gnd-section">
+        {
+          gndTables.map(function (table, i) {
+              return <Table 
+              table = {table}
+              key = {i + 3}
+              id = {i + 3}
+              onClickCell={props.onClickCell}
+              onClickIcon={props.onClickIcon}
+              />
+          })
+        }
+      </div>
+
       <TotalsTable scorecardData={props.scorecardData}/>
+
     </div>
-);
+)
+    }
 
 export default Tables;
