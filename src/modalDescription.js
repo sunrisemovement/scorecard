da0c36f7682d76ae5c73d37e0c9e7c2c0b1dbee0
colekins <colekins@gmail.com>
2019-12-03 19:25:35 -0500

    Only show GND in modal when  necessary

--- src/modalDescription.js
+++ src/modalDescription.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './modal.css';
 
 function ModalDescription(props) {
-    var isHowMuchTable = props.row.title === "Subtotal";
+
+    const isGndTable = (props.table.subtitle);
+    const isHowMuchTable = props.row.title === "Subtotal";
     const howMuchDescription = "All candidates’ tweets over a period of three months are to be considered in the data. Each candidate will be ranked based on how high their score is. The candidate with the highest overall score will receive the full 30 points and each subsequent candidate will receive a weighted score based on how they compare to the highest and lowest ranking candidates. The candidate with the lowest overall score will receive zero points from this section. Keywords for Twitter advanced search include: “climate” “climate crisis” “climate change” “climate justice” “green new deal” “#greennewdeal” “global warming”. Twitter was chosen as the primary form of social media because all candidates have more Twitter followers than Facebook or Instagram followers. Our initial assessment will focus strictly on volume of tweets with room for more analysis."
 
     return ( 
         <div className="modal-description">
             <span className="modal-table-title">
-                Green New Deal: <br></br>
+                {isGndTable && <React.Fragment>Green New Deal: <br></br> </React.Fragment>}
                 {props.table.title}
                 {props.table.subtitle}
             </span>
