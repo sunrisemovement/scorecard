101013b9ebd470b9366edf8247eac4e82c400c11
colekins <colekins@gmail.com>
2019-12-09 15:35:05 -0500

    Add svg icon, increase size and add click state

--- src/table.js
+++ src/table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './table.css';
 import icon from './assets/i.png';
+import expand from './assets/expand.svg';
 
 function Table(props) {
 
@@ -42,7 +43,7 @@ function Table(props) {
                 <td onClick={handleClick} id="biden">{biden.score}</td>
                 <td onClick={handleClick} id="warren">{warren.score}</td>
                 <td onClick={handleClick} id="sanders">{sanders.score}</td>
-                <td id="icon-cell"><img id="expand-icon" className="expand-icon" onClick={handleChevronClick} alt="Information Icon" src={icon}></img></td>
+                <td id="icon-cell"><img id="expand-icon" className="expand-icon" onClick={handleChevronClick} alt="Information Icon" src={expand}></img></td>
             </tr>
         )
     });
