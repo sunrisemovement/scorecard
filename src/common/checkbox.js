95621e8ee8a5517ed99ab5266c45720103bbbc44
colekins <colekins@gmail.com>
2019-12-18 21:43:10 -0500

    Refactor to checkbox component, add disabled class

---
+++ src/common/checkbox.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './filter.css';
+
+function Checkbox(props) {
+
+    const handleBoxClick = (e) => {
+        e.stopPropagation();
+
+        console.log(e.currentTarget)
+    }
+
+    const fullName = props.name[0] + " " + props.name[1]
+
+    const isChecked = (candidate) => {
+        return (props.filter.indexOf(candidate.toLowerCase()) > -1);
+    }
+
+    const isDisabled = (candidate) => {
+        if (candidate.toLowerCase() !== props.filterName && (props.filter.indexOf(candidate.toLowerCase()) > -1)) return true
+    }
+
+    return ( 
+        <label className={"input-container " + (isDisabled(props.name[1]) ? 'disabled' : '')}>{fullName}
+                <input type="checkbox" onChange={(handleBoxClick)} disabled={isDisabled(props.name[1])} defaultChecked={isChecked(props.name[1])}></input>
+                <span className="checkmark"></span>
+            </label>
+    );
+}
+
+export default Checkbox;
--- src/common/checkboxes.js
+++ src/common/checkboxes.js
@@ -1,53 +1,31 @@
 import React from 'react';
 import './filter.css';
+import Checkbox from './checkbox.js';
 
 function Checkboxes(props) {
 
-    // Currently selected candidates
-    // const candidateA = props.filter[0];
-    // const candidateB = props.filter[1];
-    // const candidateC = props.filter[2];
+    const candidateNames = [['Joe', 'Biden'],
+                            ['Cory', 'Booker'],
+                            ['Pete', 'Buttigieg'],
+                            ['Bernie', 'Sanders'],
+                            ['Elizabeth', 'Warren'],
+                            ['Andrew', 'Yang']]
 
-    const handleBoxClick = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        let isChecked = e.currentTarget.checked;
-        console.log(e.currentTarget)
-
-        if (!isChecked) {e.currentTarget.checked = true}
-    }
-
-    const isChecked = (candidate) => {
-        return (props.filter.indexOf(candidate) > -1);
-    }
+    const filterName = props.filter[props.ind]
 
     return ( 
         <div className="checkboxes" style={ {display: 'none'}} id={'ch-'+props.ind} onClick={(e) => e.stopPropagation()}>
-            <label className="input-container">Joe Biden
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('biden')}></input>
-                <span className="checkmark"></span>
-            </label>
-            <label className="input-container">Cory Booker
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('booker')}></input>
-                <span className="checkmark"></span>
-            </label>
-            <label className="input-container">Pete Buttigieg
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('buttigieg')}></input>
-                <span className="checkmark"></span>
-            </label>
-            <label className="input-container">Bernie Sanders
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('sanders')}></input>
-                <span className="checkmark"></span>
-            </label>
-            <label className="input-container">Elizabeth Warren
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('warren')}></input>
-                <span className="checkmark"></span>
-            </label>
-            <label className="input-container">Andrew Yang
-                <input type="checkbox" onChange={handleBoxClick} defaultChecked={isChecked('yang')}></input>
-                <span className="checkmark"></span>
-            </label>
+            {
+                candidateNames.map(function (name, i) {
+                    return <Checkbox 
+                    name = {name}
+                    filter = {props.filter}
+                    filterName = {filterName}
+                    key = {i}
+                    id = {i}
+                    />
+                })
+            }
          </div>
     );
 }
