47fb5e8420b3dd389b10593ff8ba0e592e128d2f
colekins <colekins@gmail.com>
2019-12-20 07:53:40 -0500

    Current state of filter, need to refactor state

--- src/common/checkboxes.js
+++ src/common/checkboxes.js
@@ -7,13 +7,13 @@ function Checkboxes(props) {
     // Our data doesn't currently reference the candidates
     // by their full names. We can use this constant to generate
     // necessary checkbox filters for now.
-    const candidateNames = [ ['Joe', 'Biden'],
+    const candidateNames = [['Joe', 'Biden'],
                             ['Cory', 'Booker'],
                             ['Pete', 'Buttigieg'],
                             ['Bernie', 'Sanders'],
                             ['Tom', 'Steyer'],
                             ['Elizabeth', 'Warren'],
-                            ['Andrew', 'Yang'] ]
+                            ['Andrew', 'Yang']]
 
     const filterName = props.filter[props.ind]
     var newSelection = "";
@@ -26,16 +26,7 @@ function Checkboxes(props) {
         var currentCheckboxes = document.getElementsByClassName('checkboxes')[props.ind];
         var oldCandidateBoxes = document.getElementsByClassName('box-' + filterName);
         var newCandidateBoxes = document.getElementsByClassName('box-' + newSelection);
-
-        currentCheckboxes.style.display = "none";
-
-        // if (!newSelection) {
-        //     console.log('no new')
-        //     oldCandidateBoxes[0].checked = true;
-        //     oldCandidateBoxes[1].checked = true;
-        //     oldCandidateBoxes[2].checked = true;
-        // }
-        
+        currentCheckboxes.style.display = "none";    
         
         if (newSelection && newSelection !== filterName) {
             // If a new candidate was selected, uncheck old candidate in all overlays,
@@ -52,7 +43,8 @@ function Checkboxes(props) {
             // If there's a valid new selection, update state
             props.handleFilterChange(newSelection, props.filterName, props.ind)
 
-        } else 
+        } 
+        else 
         {
             // If not, reselect the original candidate and restore 'active' class
             oldCandidateBoxes[props.ind].checked = true;
