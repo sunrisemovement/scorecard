5641b6b33209cda3a8394708b144eb2ccbadd9c7
brookeangel <brooke.nicole.angel@gmail.com>
2020-05-30 10:31:21 -0700

    Build app for gh-pages

---
+++ precache-manifest.42406e0d19fe86e1e7bb50d882734919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b48ebe7e5df9a9c9b82cc87c57eb99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7bb986237587f48c36d",
+    "url": "./static/css/main.99fb5a20.chunk.css"
+  },
+  {
+    "revision": "8df90725844e592c2c0f",
+    "url": "./static/js/2.9103076c.chunk.js"
+  },
+  {
+    "revision": "c7bb986237587f48c36d",
+    "url": "./static/js/main.fe09a846.chunk.js"
+  },
+  {
+    "revision": "84efdf1ea8c7536c3663",
+    "url": "./static/js/runtime-main.9fbeb3a0.js"
+  },
+  {
+    "revision": "4ef1776203f5e55f332fcfacd2a57b12",
+    "url": "./static/media/expand.4ef17762.svg"
+  }
+]);
